11c6fb360679e196afe2a156b7e1950ac98790f4
VÕ MINH SANG <31920328+MRCC-MC@users.noreply.github.com>
2021-08-04 22:40:02 +0700

    Update index.js
    update scale IOT

--- index.js
+++ index.js
@@ -14,28 +14,41 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/", (req, res) => {
-    const { IP, flag } = req.body;
+    const { IP, flag, iot } = req.body;
     const fileHost = './traefik-lb/traefik-dynamic.toml'
     const ipTCP = `[[http.services.service-http.loadBalancer.servers]]\nurl = "http://${IP}"`
     const ipUDP = `[[udp.services.service-udp.loadBalancer.servers]]\naddress = "${IP}:60246"`
+    const ipIOT = `[[myservice.services.service-http.loadBalancer.servers]]\nurl = "http://${IP}:3005"`
     let result = null
 
     fs.readFile(fileHost, 'utf8', function (err,data) {
         if (err) return console.log(err);
         /** Delete ip server **/
         if (flag) {
-            if (data.indexOf('#tcp') !== -1 && data.indexOf('#udp') !== -1) {
-                result = data.replace(ipTCP, "");
-                result = result.replace(ipUDP , '');
+            if (iot) {
+                if (data.indexOf('#iot') !== -1) {
+                    result = data.replace(ipIOT, "");
+                } else {
+                    result = data.replace(ipIOT, '#iot');
+                }
             } else {
-                result = data.replace(ipTCP, '#tcp');
-                result = result.replace(ipUDP, '#udp');
+                if (data.indexOf('#tcp') !== -1 && data.indexOf('#udp') !== -1) {
+                    result = data.replace(ipTCP, "");
+                    result = result.replace(ipUDP, '');
+                } else {
+                    result = data.replace(ipTCP, '#tcp');
+                    result = result.replace(ipUDP, '#udp');
+                }
             }
             console.log('delete IP:', ipTCP, ipUDP)
         } else { /** Add ip server **/
-            result = data.replace(/#udp/g ,ipUDP + '\n#udp');
-            result = result.replace(/#tcp/g ,ipTCP + '\n#tcp');
-            console.log('add IP: ', ipUDP, ipTCP)
+            if (iot) {
+                result = data.replace(/#iot/g, ipIOT + '\n#iot');
+            } else {
+                result = data.replace(/#udp/g, ipUDP + '\n#udp');
+                result = result.replace(/#tcp/g, ipTCP + '\n#tcp');
+                console.log('add IP: ', ipUDP, ipTCP)
+            }
         }
         console.log('result: ', result)
         fs.writeFile(fileHost, result, 'utf8', function (err) {
